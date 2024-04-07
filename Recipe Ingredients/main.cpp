d35c7970bc6fcf9922bb2429cc1d22e38bc3459d
Daniel Holmes <dan.r.holmes@gmail.com>
2024-04-08 07:36:03 +0930

    Initial Commit

---
+++ Recipe Ingredients/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Recipe Ingredients
+//
+//  Created by Daniel Holmes on 8/4/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
